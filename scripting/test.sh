bc62a8e14e07f570352c1cd69807ec40fb5cfad9
VossMar <vossi.93@hotmail.de>
2015-11-05 14:46:00 +0100

    atrsh2

--- scripting/test.sh
+++ scripting/test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-var="Hallo Hanswurst!"
-
-echo $var
-
-ls
+read -p "Geben Sie die zu findende Datei ein: " eingabe
+if [[ -e $eingabe ]]
+then
+	echo "Datei: ${eingabe} gefunden"
+else
+	echo "Datei: ${eingabe} nicht gefunden. Wird neu angelegt!"
+	read -p "Soll die Datei neu angelegt werden?(y/n): " zust
+fi
+if [[ $zust = 'y' ]]
+then
+	touch $eingabe
+	ls -l
+fi
